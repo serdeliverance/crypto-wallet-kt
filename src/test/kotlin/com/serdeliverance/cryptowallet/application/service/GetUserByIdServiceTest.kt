67ef3ee96392d3c04e835d98f97fe21f3a7ffe7a
Sergio Cano <canosergio90@gmail.com>
2022-06-01 11:56:27 -0300

    get user by id

--- src/test/kotlin/com/serdeliverance/cryptowallet/application/service/GetUserByIdServiceTest.kt
+++ src/test/kotlin/com/serdeliverance/cryptowallet/application/service/GetUserByIdServiceTest.kt
@@ -2,24 +2,24 @@ package com.serdeliverance.cryptowallet.application.service
 
 import com.serdeliverance.cryptowallet.application.port.out.UserRepositoryPort
 import com.serdeliverance.cryptowallet.v2.domain.User
-import io.mockk.every
-import io.mockk.mockk
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
 import org.spekframework.spek2.Spek
 
 object GetUserByIdServiceTest : Spek({
 
-    val userRepository = mockk<UserRepositoryPort>()
+    val userRepository = mock<UserRepositoryPort>()
     val subject = GetUserByIdService(userRepository)
 
     test("get user by id") {
-        every { userRepository.find(2) } returns User(2, "pepe", "asd123as", "pepe@gmail.com")
+        whenever(userRepository.find(2)).thenReturn(User(2, "pepe", "asd123as", "pepe@gmail.com"))
         val result = subject.getUserById(2)
         assertEquals(User(2, "pepe", "asd123as", "pepe@gmail.com"), result)
     }
 
     test("get user not find user") {
-        every { userRepository.find(2) } returns null
+        whenever(userRepository.find(2)).thenReturn(null)
         val result = subject.getUserById(2)
         assertEquals(null, result)
     }
