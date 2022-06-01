a7e14fb112777f8c41d4148a77c18fe3699d28e9
Sergio Cano <canosergio90@gmail.com>
2022-06-01 11:48:43 -0300

    get user by id - test running (with warnings)

--- src/test/kotlin/com/serdeliverance/cryptowallet/application/service/GetUserByIdServiceTest.kt
+++ src/test/kotlin/com/serdeliverance/cryptowallet/application/service/GetUserByIdServiceTest.kt
@@ -2,29 +2,25 @@ package com.serdeliverance.cryptowallet.application.service
 
 import com.serdeliverance.cryptowallet.application.port.out.UserRepositoryPort
 import com.serdeliverance.cryptowallet.v2.domain.User
-import io.kotest.core.spec.style.WordSpec
-import io.kotest.matchers.shouldBe
 import io.mockk.every
 import io.mockk.mockk
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.spekframework.spek2.Spek
 
-class GetUserByIdServiceTest : WordSpec({
+object GetUserByIdServiceTest : Spek({
 
     val userRepository = mockk<UserRepositoryPort>()
     val subject = GetUserByIdService(userRepository)
 
-    "GetUserByIdService" When {
-        "founds user" Should {
-            every { userRepository.find(2) } returns User(2, "pepe", "asd123as", "pepe@gmail.com")
-            val result = subject.getUserById(2)
+    test("get user by id") {
+        every { userRepository.find(2) } returns User(2, "pepe", "asd123as", "pepe@gmail.com")
+        val result = subject.getUserById(2)
+        assertEquals(User(2, "pepe", "asd123as", "pepe@gmail.com"), result)
+    }
 
-            result shouldBe User(2, "pepe", "asd123as", "pepe@gmail.com")
-        }
-
-        "not found user" Should {
-            every { userRepository.find(2) } returns null
-            val result = subject.getUserById(2)
-
-            result shouldBe null
-        }
+    test("get user not find user") {
+        every { userRepository.find(2) } returns null
+        val result = subject.getUserById(2)
+        assertEquals(null, result)
     }
 })
