74199ac19ea2fd3d00eb50d036fa454e387debb9
Sergio Cano <canosergio90@gmail.com>
2022-06-09 18:48:19 -0300

    defining domain and usecase interface

---
+++ src/main/kotlin/com/serdeliverance/cryptowallet/application/port/in/TransferMoneyUseCase.kt
@@ -0,0 +1,9 @@
+package com.serdeliverance.cryptowallet.application.port.`in`
+
+import com.serdeliverance.cryptowallet.domain.Cryptocurrency
+import com.serdeliverance.cryptowallet.v2.domain.User
+import java.math.BigDecimal
+
+interface TransferMoneyUseCase {
+    fun transfer(issuer: User, receiver: User, cryptocurrency: Cryptocurrency, amount: BigDecimal)
+}
---
+++ src/main/kotlin/com/serdeliverance/cryptowallet/v2/domain/Transaction.kt
@@ -0,0 +1,14 @@
+package com.serdeliverance.cryptowallet.v2.domain
+
+import com.serdeliverance.cryptowallet.domain.OperationType
+import java.math.BigDecimal
+import java.time.LocalDateTime
+
+data class Transaction(
+    val id: Long,
+    val userId: Int,
+    val cryptoCurrencyId: Int,
+    val amount: BigDecimal,
+    val operationType: OperationType,
+    val transactionDate: LocalDateTime
+)
